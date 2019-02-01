8471a72543743c4d78e8e5f83f79972a13154e59
Nathania Devina Godjali <devinagodjali@gmail.com>
2019-02-01 14:07:11 +0700

    Updates

---
+++ precache-manifest.89fedde829c7dce933c36e015f4215fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7da090716cc526bfe68",
+    "url": "/starwars_front-end-test.git/static/css/main.e92fb882.chunk.css"
+  },
+  {
+    "revision": "e7da090716cc526bfe68",
+    "url": "/starwars_front-end-test.git/static/js/main.e7da0907.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/starwars_front-end-test.git/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "e5866d5e65c4974e9078",
+    "url": "/starwars_front-end-test.git/static/js/runtime~main.e5866d5e.js"
+  },
+  {
+    "revision": "eb906ca7d153c32b5d1e7a841ccb8766",
+    "url": "/starwars_front-end-test.git/static/media/logo.eb906ca7.png"
+  },
+  {
+    "revision": "d7a61190a836bdcfc62bf97af4f4c74b",
+    "url": "/starwars_front-end-test.git/static/media/stars.d7a61190.png"
+  },
+  {
+    "revision": "e2da85bcd04ddda3e358902790bb06db",
+    "url": "/starwars_front-end-test.git/static/media/starwars.e2da85bc.jpg"
+  },
+  {
+    "revision": "0c441fc16cc2c2b5e3e1026515744768",
+    "url": "/starwars_front-end-test.git/index.html"
+  }
+];
